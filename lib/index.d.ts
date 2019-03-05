8f34cc18dfb2e5a146c7d50d80a4050650d03d05
Anil Anar <anilanar@hotmail.com>
2019-03-05 22:53:46 +0100

    gitpkg

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+export * from "./Contracts";
+export * from "./DynamicModuleLoader";
+export * from "./ModuleStore";
+export * from "./Utils/ComparableMap";
+export * from "./Utils/RefCounter";
+export * from "./Managers/MiddlewareManager";
+export * from "./Managers/RefCountedManager";
